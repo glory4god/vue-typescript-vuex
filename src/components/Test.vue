8d0d5ed9529468a8909de01fdd0e45ed6f6942b3
Hayoung <gkduddy0814@gmail.com>
2021-07-25 23:26:43 +0900

    feat : first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>TEST page</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Test',
+});
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>ㅎㅇㅎㅇ</h2>
+    <Test></Test>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Test from '@/components/Test.vue';
+
+export default defineComponent({
+  name: 'About',
+  components: {
+    Test,
+  },
+});
+</script>
