2a3eb800dfbe918fbabe126ca3224788d0afe471
Hayoung <gkduddy0814@gmail.com>
2021-07-26 12:05:41 +0900

    feat : add interval vuex actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,56 @@
 import { createStore } from 'vuex';
 import { Record } from '@/views/Test.vue';
+import { getData } from '@/lib/testSheet';
+
 export default createStore({
   state: {
-    currentWord: '시작',
+    currentWord: '시작' as string,
     recordList: [] as Record[],
+    numberList: [] as number[],
+    randomList: [] as number[],
   },
   mutations: {
-    setWord(state, word) {
+    setWord(state, word: string) {
       state.currentWord = word;
     },
-    addRecordList(state, record) {
+    addRecordList(state, record: Record) {
       state.recordList.push(record);
     },
     reset(state) {
       state.currentWord = '시작';
       state.recordList = [];
     },
+    getData(state, data: number[]) {
+      state.numberList = data;
+    },
+    addRandomList(state, number: number) {
+      state.randomList.push(number);
+    },
   },
   getters: {
-    lengthToWord: (state) => {
+    lengthToWord(state): number {
       return state.currentWord.length;
     },
   },
-  actions: {},
+  actions: {
+    addRecordListAsync(context, record: Record) {
+      setTimeout(() => {
+        context.commit('addRecordList', record);
+      }, 500);
+    },
+    async gotData(context) {
+      context.commit('getData', await getData());
+    },
+
+    interval(context) {
+      const time = setInterval(() => {
+        context.commit('addRandomList', Math.floor(Math.random() * 9 + 1));
+        console.log(context.state.randomList);
+        if (context.state.randomList.length >= 10) {
+          clearInterval(time);
+        }
+      }, 1000);
+    },
+  },
   modules: {},
 });
